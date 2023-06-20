5302e849bdd060daed795dd9b5bb7a25da7ef603
Eclipsorum <eclipsorum@gmail.com>
2023-06-20 23:00:04 +0700

    Finish exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let myArr = args[0];
+    for(let i=1;i<args.length;i++){
+        if(myArr.findIndex(element => element === args[i]) !== -1) {
+        myArr.splice(myArr.findIndex(element => element === args[i]),1);
+        }
+    }
 
+    return myArr;
 };
 
 // Do not edit below this line
