3c76c907e679bf016365d7d34dcf577761bfeebd
Eclipsorum <eclipsorum@gmail.com>
2023-09-24 18:37:56 +0700

    Finish exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((accu, item) => accu + item,0);
+};
+
+const multiply = function(a, ...b) {
+  let result = a;
+  for (let arg of b) {
+    result *= arg;
+  };
+  return result;
+};
+
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let arr = [];
+	for (let i = a; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr.reduce((accu, item) => accu * item, 1);
 };
 
 // Do not edit below this line
