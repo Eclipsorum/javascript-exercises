9894054cadfe46c3ce6b6e5be6b6fd8ad75081a8
Eclipsorum <eclipsorum@gmail.com>
2023-09-25 20:41:10 +0700

    Finish exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strReverse = str.replace(/\W/g, '').toLowerCase().split('').reverse();
+    return strReverse.every((item, index) =>  item === str.replace(/\W/g, '').toLowerCase().split('')[index]);
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
