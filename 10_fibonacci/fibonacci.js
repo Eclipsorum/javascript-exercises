138342beb54dc6de7514d99169da503bfe7ef921
Eclipsorum <eclipsorum@gmail.com>
2023-09-25 21:59:21 +0700

    Finish exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let fib = 0, prevFib = 0;
+    for(let i = n; i > 0; i--) {
+        fib == 0 ? fib++ : fib = fib + prevFib;
+        i == n ? "" : prevFib = fib - prevFib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
