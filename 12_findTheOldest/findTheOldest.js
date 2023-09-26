3da93ea21f344a84d9fc26202141f494c70da1b2
Eclipsorum <eclipsorum@gmail.com>
2023-09-26 13:58:26 +0700

    Finish exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((accu, item) => {
+        let accuDeath = accu.yearOfDeath, itemDeath = item.yearOfDeath;
+        if(accuDeath === undefined) { accuDeath = new Date().getFullYear(); }
+        if(itemDeath === undefined) { itemDeath = new Date().getFullYear(); }
+        return (accuDeath - accu.yearOfBirth) > (itemDeath - item.yearOfBirth) ? accu : item          
+    });
 };
 
 // Do not edit below this line
